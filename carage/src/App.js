87d089065d3887fec2c2ce30606bbe2e7c1f94ad
Ibrahimt Tameme <ibrahimtameme98@gmail.com>
2022-07-07 09:17:18 +0300

    single

--- carage/src/App.js
+++ carage/src/App.js
@@ -1,7 +1,10 @@
 import './style.css';
+
 import Navbar from './Navbar';
 import Home from './Home';
 import Footer from './Footer';
+import Car_wash from './Components/Singlepages/Car_wash/Car_wash';
+import Charger from './Components/Singlepages/Charger/Charger';
 
 
 
@@ -9,6 +12,10 @@ function App() {
   return (
     <div className="App">
       <>
+      
+<Car_wash/>
+<Charger/>
+
       <Navbar/>
       <Home/>
       <Footer/>
