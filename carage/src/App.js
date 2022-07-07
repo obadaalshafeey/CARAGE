8678fd914ab938ff105f11c769124d0b1cdc357f
majdalbalawneh <majd3180157@gmail.com>
2022-07-07 11:45:40 +0300

    get

--- carage/src/App.js
+++ carage/src/App.js
@@ -1,8 +1,15 @@
 import './style.css';
+
 import Navbar from './Navbar';
 import Home from './Home';
 import Contact from './Contact';
 import Footer from './Footer';
+import Car_wash from './Components/Singlepages/Car_wash/Car_wash';
+import Charger from './Components/Singlepages/Charger/Charger';
+import About from './About';
+import {Routes,BrowserRouter, Route,Link} from "react-router-dom";
+// import './js/main'
+// import {$,jQuery} from 'jquery';
 import Services from './Services';
 import Wash from './Wash';
 // import Example from './Form';
@@ -12,18 +19,19 @@ import Wash from './Wash';
 
 function App() {
   return (
-    <div className="App">
       <>
-      <Navbar/>
-      <Wash/>
-      {/* <Example/> */}
-      {/* <Home/> */}
-      {/* <Contact /> */}
-      {/* <Services/> */}
-      
-      <Footer/>
-      </>
-    </div>
+    <BrowserRouter>
+    <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/Services" element={<Services />}/>
+        <Route path="/About" element={<About />}/>
+        <Route path="/Contact" element={<Contact />}/>
+      </Routes>
+    </BrowserRouter>
+
+    <Footer/>
+    </>
   );
 }
 
