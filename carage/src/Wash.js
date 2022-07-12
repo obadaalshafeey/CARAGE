4525692fefaa4cb3efc30e4b07899328af96f2d8
marwanseerat <marwanseerat06@gmail.com>
2022-07-13 00:09:27 +0300

    wash form service

--- carage/src/Wash.js
+++ carage/src/Wash.js
@@ -1,52 +1,30 @@
 import React from 'react';
 // import Car_wash from './Form';
 import Form from './Form';
-// import { Link } from 'react-router-dom';
+import Header from './Components/Header';
+import { Link } from 'react-router-dom';
 
 
 
 function Wash (){
     return(      
 <>
-{/* //////////////// */}
-<div className="page-header">
-    <div className="container">
-      <div className="row">
-        <div className="col-12">
-          <h2>Single Page</h2>
-        </div>
-        <div className="col-12">
-          <a href="">Home</a>
-          <a href="">Single Page</a>
-        </div>
-      </div>
-    </div>
-  </div>
+{/* Page Header start */}
+<Header title="Singl Service"/>
   {/* Page Header End */}
-  {/* About Start */}
-  <div className="about">
+  
+ {/* services start */}
+ <div className="about">
     <div className="container">
       <div className="row align-items-center">
         <div className="col-lg-6">
           <div className="about-img">
-
-          <div>
-    {/* <iframe src='../Wash.mp4'
-  
-        frameborder='0'
-        allow='autoplay; encrypted-media'
-        allowfullscreen
-        title='video'
- width={'90%'} height={'90%'}/> */}
- </div>
-
-
-            <img src="../img/carwash.png" alt="Image" />
+            <img src="./img/about2.jpg" alt="Image" />
           </div>
         </div>
         <div className="col-lg-6">
           <div className="section-header text-left">
-            <p>Wash</p>
+            <p>Service</p>
             <h2>car washing and detailing</h2>
           </div>
           <div className="about-content">
@@ -71,252 +49,26 @@ function Wash (){
                 Decorating cars for occasions
               </li>
             </ul>
-            {/* <Link to='/Services'> */}
-            {/* <a className="btn btn-custom" href=""  >
-            </a> */}
-
+            
             <Form/>
-            {/* </Link> */}
           </div>
         </div>
       </div>
     </div>
   </div>
-{/* /////////////////// */}
- 
-{/* /////Form */}
-
-
-
-
-{/* Price Start */}
-<div className="price">
-    <div className="container">
-      <div className="section-header text-center">
-        <p>Washing Plan</p>
-        <h2>Choose Your Plan</h2>
-      </div>
-      <div className="row">
-        <div className="col-md-4">
-          <div className="price-item">
-            <div className="price-header">
-              <h3>Basic Cleaning</h3>
-              <h2>
-                <span>$</span>
-                <strong>25</strong>
-                <span>.99</span>
-              </h2>
-            </div>
-            <div className="price-body">
-              <ul>
-                <li>
-                  <i className="far fa-check-circle" />
-                  Seats Washing
-                </li>
-                <li>
-                  <i className="far fa-check-circle" />
-                  Vacuum Cleaning
-                </li>
-                <li>
-                  <i className="far fa-check-circle" />
-                  Exterior Cleaning
-                </li>
-                <li>
-                  <i className="far fa-times-circle" />
-                  Interior Wet Cleaning
-                </li>
-                <li>
-                  <i className="far fa-times-circle" />
-                  Window Wiping
-                </li>
-              </ul>
-            </div>
-            <div className="price-footer">
-              <a className="btn btn-custom" href="">
-                Book Now
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="col-md-4">
-          <div className="price-item featured-item">
-            <div className="price-header">
-              <h3>Premium Cleaning</h3>
-              <h2>
-                <span>$</span>
-                <strong>35</strong>
-                <span>.99</span>
-              </h2>
-            </div>
-            <div className="price-body">
-              <ul>
-                <li>
-                  <i className="far fa-check-circle" />
-                  Seats Washing
-                </li>
-                <li>
-                  <i className="far fa-check-circle" />
-                  Vacuum Cleaning
-                </li>
-                <li>
-                  <i className="far fa-check-circle" />
-                  Exterior Cleaning
-                </li>
-                <li>
-                  <i className="far fa-check-circle" />
-                  Interior Wet Cleaning
-                </li>
-                <li>
-                  <i className="far fa-times-circle" />
-                  Window Wiping
-                </li>
-              </ul>
-            </div>
-            <div className="price-footer">
-              <a className="btn btn-custom" href="">
-                Book Now
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="col-md-4">
-          <div className="price-item">
-            <div className="price-header">
-              <h3>Complex Cleaning</h3>
-              <h2>
-                <span>$</span>
-                <strong>49</strong>
-                <span>.99</span>
-              </h2>
-            </div>
-            <div className="price-body">
-              <ul>
-                <li>
-                  <i className="far fa-check-circle" />
-                  Seats Washing
-                </li>
-                <li>
-                  <i className="far fa-check-circle" />
-                  Vacuum Cleaning
-                </li>
-                <li>
-                  <i className="far fa-check-circle" />
-                  Exterior Cleaning
-                </li>
-                <li>
-                  <i className="far fa-check-circle" />
-                  Interior Wet Cleaning
-                </li>
-                <li>
-                  <i className="far fa-check-circle" />
-                  Window Wiping
-                </li>
-              </ul>
-            </div>
-            <div className="price-footer">
-              <a className="btn btn-custom" href="">
-                Book Now
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  {/* Price End */}
-
-<br></br>
-
-
-     {/* Other Services */}
-     
+  {/* services End */}
   
-     <>
-       
-       <>
-  <meta charSet="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Document</title>
-  <style
-    dangerouslySetInnerHTML={{
-      __html:
-        "\n@import url('https://fonts.googleapis.com/css?family=Poppins|Ubuntu&display=swap');\n\n\n*\n{\n    box-sizing: border-box;\n    margin: 0;\n    padding: 0;\n}\nbody\n{\n    background: #fff;\n    font-family: 'Ubuntu', sans-serif;\n\n}\n\n.services\n{\n    display: block;\n    position: relative;\n    padding: 0;\n\n}\n.container\n{\n    max-width: 1200px;\n    padding-right: 15px;\n    padding-left: 15px;\n    margin-right: auto;\n    margin-left: auto;\n}\n\n.services-title\n{\n    display: flex;\n    flex-wrap: wrap;\n    margin-right: -15px;\n    margin-left: -15px;\n    \n}\n.services-box\n{\n    display: flex;\n    flex-wrap: wrap;\n    margin-right: -15px;\n    margin-left: -15px;\n    margin-top: -15px;\n    \n}\n\n.title{\n    margin-left: 15px;\n    position: relative;\n    width: 100%;\n    padding-right: 15px;\n    padding-left: 15px;\n    margin-bottom: 40px;\n    text-align: center;\n    top: 26px;\n}\n/**\n.services-title>span {\n    height: 36px;\n    width: 130px;\n    margin: 0 auto;\n    margin-bottom: 20px;\n    display: inline-block;\n    font-size: 20px;\n    color:rgb(255, 74, 50);\n \n}****/\n.services-title h2 {\n    margin-top: -10px;\n    margin-bottom: 6px;\n    text-transform: capitalize;\n    font-size: 36px;\n    line-height: 48px;\n    font-weight: 700;\n    color: #333;\n    font-family: 'ubuntu',sans-serif;\n\n}\n.services-title h2 span\n{\n    color: #3c44a9;\n}\n\n\n.box\n{\n    position: relative;\n    width: 100%;\n    padding-right: 15px;\n    padding-left: 15px;\n    max-width: 33.333333%;\n\n}\n.ser-box\n{    margin-top: 75px;\n    padding: 0 60px;\n    padding-bottom: 30px;\n    min-height: 198px;\n    text-align: center;\n    border-radius: 10px;\n    background-color: #fff;\n    -webkit-transition: all .4s ease-in-out;\n    -moz-transition: all .4s ease-in-out;\n    -ms-transition: all .4s ease-in-out;\n    -o-transition: all .4s ease-in-out;\n    transition: all .4s ease-in-out;\n    -webkit-box-shadow: 0 0 25px 0 rgba(20,27,201,.17);\n    box-shadow: 0 0 25px 0 rgba(20,27,201,.17);\n}\n\n.ser-box .icon\n{\n    margin-bottom: 0;\n    -webkit-transform: translateY(-50%);\n    -ms-transform: translateY(-50%);\n    transform: translateY(-50%);\n    text-align: center;\n}\n.ser-box h4\n{\n    font-size: 20px;\n    line-height: 1;\n    font-weight: 700;\n    color: #3c44a9;\n    margin-top: -10px;\n    font-family: 'Ubuntu', sans-serif;\n\n}\n.ser-box p {\n    color: #61648e;\n    line-height: 24px;\n    margin-top: 20px;\n    font-family: 'Ubuntu', sans-serif;\n\n}\n\n\n.ser-box:hover {\n background-image:-moz-linear-gradient(-45deg,#fb0054 0%,#f55b2a 100%);\n background-image:-webkit-linear-gradient(-45deg,#fb0054 0%,#f55b2a 100%);\n background-image:-ms-linear-gradient(-45deg,#fb0054 0%,#f55b2a 100%);\n -webkit-box-shadow:0 0 25px 0 rgba(20,27,201,.05);\n box-shadow:0 0 25px 0 rgba(20,27,201,.05);\n cursor:pointer\n}\n\n.ser-box:hover h4,\n.ser-box:hover p {\n color:#fff\n}\n\n\n\n.services-box .box:nth-child(1) .ser-box:hover {\n background-image:-webkit-linear-gradient(-45deg,#34b5bf 0%,#210c59 100%);\n background-image:linear-gradient(-45deg,#34b5bf 0%,#210c59 100%)\n}\n.services-box .box:nth-child(3) .ser-box:hover {\n background-image:-webkit-linear-gradient(-45deg,#3615e7 0%,#44a2f6 100%);\n background-image:linear-gradient(-45deg,#3615e7 0%,#44a2f6 100%)\n}\n.services-box .box:nth-child(4) .ser-box:hover{\n background-image:-webkit-linear-gradient(-45deg,#fc6a0e 0%,#fdb642 100%);\n background-image:linear-gradient(-45deg,#fc6a0e 0%,#fdb642 100%);\n}\n.services-box .box:nth-child(5) .ser-box:hover {\n background-image:linear-gradient(-45deg,#8d40fb 0%,#5a57fb 100%);\n background-image:-webkit-linear-gradient(-45deg,#8d40fb 0%,#5a57fb 100%);\n\n}\n.services-box .box:nth-child(6) .ser-box:hover {\n background-image:-webkit-linear-gradient(-45deg,#27b88d 0%,#22dd73 100%);\n background-image:linear-gradient(-45deg,#27b88d 0%,#22dd73 100%)\n}\n\n\n\n\n\n\n@media (min-width: 768px){\n.box \n{\n    flex: 0 0 50% !important;\n    max-width: 50% !important;\n}\n\n}\n\n\n\n@media (min-width: 600px){\n.box \n{\n    flex: 0 0 50% !important;\n    max-width: 50% !important;\n}\n\n}\n\n\n\n\n\n@media (min-width: 992px)\n{\n.box \n{\n    flex: 0 0 33.333333% !important;\n    max-width: 33.333333% !important;\n}\n}\n\n\n@media (min-width: 414px){\n.box \n{\n    flex: 0 0 100%;\n    max-width: 100%;\n}\n\n}\n\n@media (min-width: 370px){\n.box \n{\n    flex: 0 0 100%;\n    max-width: 100%;\n}\n\n}\n\n@media (min-width: 320px){\n.box \n{\n    flex: 0 0 100%;\n    max-width: 100%;\n}\n\n}\n\n\n\n\n\n\n\n"
-    }}
-  />
-  <section className="services">
-    <div className="container">
-      <div className="services-title">
-        <div className="title">
-          {/*-- <span>S</span> ---*/}
-          <h2>
-            <span> Other </span> Services
-          </h2>
-        </div>
-      </div>
-      <div className="services-box">
-        <div className="box">
-          <div className="ser-box">
-            <div className="icon">
-              <img src="../img/8.png" alt='setting' width={'30%'}/>
-            </div>
-            <h4>Car Repair</h4>
-            <p>We’re big proponents of proper maintenance and service.</p>
-          </div>
-        </div>
-        <div className="box">
-          <div className="ser-box">
-            <div className="icon">
-              <img src="../img/15.png" alt="speed" width={'30%'}/>
-            </div>
-            <h4>Car Decoration</h4>
-            <p>We’re big proponents of proper maintenance and service..</p>
-          </div>
-        </div>
-        <div className="box">
-          <div className="ser-box">
-            <div className="icon">
-              <img src="../img/13.png" alt="protect" width={'30%'} />
-            </div>
-            <h4>Car Rentals</h4>
-            <p>We’re big proponents of proper maintenance and service..</p>
-          </div>
-        </div>
-        <div className="box">
-          <div className="ser-box">
-            <div className="icon">
-              <img src="../img/14.png" alt="backup" width={'30%'}/>
-            </div>
-            <h4>Monthly Backup</h4>
-            <p>We’re big proponents of proper maintenance and service..</p>
-          </div>
-        </div>
-        <div className="box">
-          <div className="ser-box">
-            <div className="icon">
-              <img src="../img/10.png" alt="img4" width='30%'/>
-            </div>
-            <h4>Free SSL Certificate</h4>
-            <p>We’re big proponents of proper maintenance and service..</p>
-          </div>
-        </div>
-        {/* <div className="box">
-          <div className="ser-box">
-            <div className="icon">
-              <img src="../img/11.png" alt='database' width={'30%'}/>
-            </div>
-            <h4>car wash</h4>
-            <p>We’re big proponents of proper maintenance and service..</p>
-          </div>
-        </div> */}
-      </div>
-    </div>
-  </section>
-</>
+
+
+
 
 
 </>
 
-</>
+
+
+
+
 
     );
 }
